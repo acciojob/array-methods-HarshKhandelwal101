9aec2769e489d2de7ac665459d3c085b483be999
HarshKhandelwal101 <78348500+HarshKhandelwal101@users.noreply.github.com>
2023-03-06 20:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-Array.prototype.myMap = function (callback) {
-  // Write your code here.
-};
-
-Array.prototype.myFilter = function (callback) {
-  // Write your code here.
-};
-
-Array.prototype.myReduce = function (callback, initialValue) {
-  // Write your code here.
-};
+Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this));
+    }
+    return newArray;
+  };
+  
+  Array.prototype.myFilter = function(callback) {
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        newArray.push(this[i]);
+      }
+    }
+    return newArray;
+  };
+  
+  Array.prototype.myReduce = function(callback, initialValue) {
+    let accumulator = initialValue !== undefined ? initialValue : this[0];
+    for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+    return accumulator;
+  };
+  
+  
+module.exports = Array.prototype;
